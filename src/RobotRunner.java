8ffd502d357df829d819c8c573800f6938525a05
BT_1N3_15 <sample@bths.edu>
2024-03-12 09:04:07 -0400

    Initial Commit

---
+++ src/RobotRunner.java
@@ -0,0 +1,7 @@
+public class RobotRunner {
+    public static void main(String[] args) {
+        System.out.println("☐");
+        System.out.println("⚡"); //lightning bolt for robot
+        System.out.println("\uD83D\uDCA7"); //water
+    }
+}
---
+++ src/Shuimen.java
@@ -0,0 +1,5 @@
+//shuimen - water people, using the chinese word for water
+//robots fear water, Shuimen will destroy any robot it makes contact with
+public class Shuimen {
+
+}
